4a2a0210fbe8a91d6fb48fb3cc2af4bf2bce5c39
zSenTnzY <zsentnzy@gmail.com>
2026-07-19 00:19:47 +0000

    render delay mobile test7bis

--- src/routes/test.tsx
+++ src/routes/test.tsx
@@ -1,6 +1,6 @@
 import { createFileRoute } from "@tanstack/react-router";
 
-export const Route = createFileRoute("/test-image")({
+export const Route = createFileRoute("/test")({
   component: TestImage,
 });
 
